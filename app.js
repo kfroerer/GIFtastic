9a21a33126fcc83b32f5ed33cf710e5febd06d6e
kjojones <43149867+kfroerer@users.noreply.github.com>
2018-11-06 06:17:01 -0600

    still gif images added

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
-var topic = ["Cinderella", "Pocohantas", "Frozen", "The Little Mermaid", "Toy Story", "Moana"]
+var topic = ["Bye", "Good Morning", "love you", "so tired", "excited", "tgif"]
 for (var j = 0; j < topic.length; j++){
-    var movieButton = $("<button>").addClass("button movieButton");
-    movieButton.attr("data-title", topic[j]);
-    movieButton.text(topic[j]);
-    $("#button-row").append(movieButton);
+    var phraseButton = $("<button>").addClass("button phraseButton");
+    phraseButton.attr("data-title", topic[j]);
+    phraseButton.text(topic[j]);
+    $("#button-row").append(phraseButton);
 };
 
 
 $("button").on("click", function(){
     var search = $(this).attr("data-title")
-    var queryUrl = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=spWh9C8hZ8YYZbe5GmdARig3L7FNyNFK&limit=5";
+    var queryUrl = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=spWh9C8hZ8YYZbe5GmdARig3L7FNyNFK&limit=10";
     
     $.ajax({
         url: queryUrl,
@@ -19,26 +19,25 @@ $("button").on("click", function(){
         console.log(response);
         // var results = response.data;
         var results = response.data;
+        
+        for (var i = 0; i < results.length; i++) {
+            var gifDiv = $("<div>");
+            
+            var rating = results[i].rating;
+            
+            var p = $("<p>").text("Rating: " + rating);
+            
+            var gifImage = $("<img>");
+            gifImage.attr("src", results[i].images.fixed_height_still.url);
+            
+            gifDiv.prepend(p);
+            gifDiv.prepend(gifImage);
+            
+            $("#gifs-go-here").prepend(gifDiv);
+        }
     });
 });
-})
-
-//         for (var i = 0; i < results.length; i++) {
-//         var gifDiv = $("<div>");
-
-//         var rating = results[i].rating;
-
-//         var p = $("<p>").text("Rating: " + rating);
-
-//         var personImage = $("<img>");
-//         personImage.attr("src", results[i].images.fixed_height.url);
-
-//         gifDiv.prepend(p);
-//         gifDiv.prepend(personImage);
-
-//         $("#gifs-go-here").prepend(gifDiv);
-//         }
-//     });
+});
 // });//end of button click event
 
 // })
