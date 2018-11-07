2aaa8f651a5f2ad901df299719e22fa01edafda6
kjojones <43149867+kfroerer@users.noreply.github.com>
2018-11-06 22:09:33 -0600

    added bootstrap, input field

--- app.js
+++ app.js
@@ -6,11 +6,17 @@ for (var j = 0; j < topic.length; j++){
     phraseButton.text(topic[j]);
     $("#button-row").append(phraseButton);
 };
-
+// not staying in the button row
+// $(document).on("click", "#save-gif", function(){
+//     var userInput = $("#input").val().trim();
+//     var userAddedButton = $("<button>");
+//     userAddedButton.text(userInput)
+//     $("#button-row").append(userAddedButton);
+// });
 
 $("button").on("click", function(){
     var search = $(this).attr("data-title")
-    var queryUrl = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=spWh9C8hZ8YYZbe5GmdARig3L7FNyNFK&limit=10";
+    var queryUrl = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=spWh9C8hZ8YYZbe5GmdARig3L7FNyNFK&limit=10&rating=g&rating=pg";
     
     $.ajax({
         url: queryUrl,
@@ -28,7 +34,7 @@ $("button").on("click", function(){
             var p = $("<p>").text("Rating: " + rating);
             
             var gifImage = $("<img>");
-            gifImage.attr("src", results[i].images.fixed_height_still.url);
+            gifImage.attr("src", results[i].images.fixed_height.url);
             
             gifDiv.prepend(p);
             gifDiv.prepend(gifImage);
@@ -37,7 +43,19 @@ $("button").on("click", function(){
         }
     });
 });
-});
+
+// $("img").on("click", function(){
+    //need to access to for loop... do another one? 
+    //change gif img attr to ("src", results[i].images.fixed_height.url)
+// })
+
+// on <img> click,
+//  if still, change img to fixed_height
+//  if animated, change img to fixed_height_still
+
+
+
 // });//end of button click event
 
 // })
+});
