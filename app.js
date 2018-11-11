38eed08a3bc0685a83309a7b87574cdbb5f35347
kjojones <43149867+kfroerer@users.noreply.github.com>
2018-11-10 21:40:08 -0600

    added 10 more gif functionality

--- app.js
+++ app.js
@@ -1,43 +1,31 @@
 $(document).ready(function(){
-var topic = ["Bye", "Good Morning", "love you", "so tired", "excited", "tgif"]
-var renderButtons = function(){
-for (var j = 0; j < topic.length; j++){
-    // $("#gifs-go-here").empty();
-    var phraseButton = $("<button>").addClass("buttons phraseButton");
-    phraseButton.attr("data-title", topic[j]);
-    phraseButton.text(topic[j]);
-    $("#button-row").append(phraseButton);
-};
-};
-renderButtons();
-
-$("#save-gif").on("click", function(event) {
-    event.preventDefault();
-    $("#button-row").empty();
-    var newGIF = $("#input").val().trim();
-    topic.push(newGIF);
+    var topic = ["Bye", "Good Morning", "love you", "so tired", "excited", "tgif"]
+    var renderButtons = function(){
+    for (var j = 0; j < topic.length; j++){
+        // $("#gifs-go-here").empty();
+        var phraseButton = $("<button>").addClass("buttons phraseButton");
+        phraseButton.attr("data-title", topic[j]);
+        phraseButton.attr("data-click", 0);        
+        phraseButton.text(topic[j]);
+        $("#button-row").append(phraseButton);
+    };
+    };
     renderButtons();
-})
-// var animatedGIF;
-// var stillGIF;
-var gifImage;
-$(document).on("click", ".buttons", function(){
-    var search = $(this).attr("data-title")
-    var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=spWh9C8hZ8YYZbe5GmdARig3L7FNyNFK&limit=10&rating=g&rating=pg";
     
-    $.ajax({
-        url: queryUrl,
-        method: "GET"
-    }).then(function(response){
-        console.log(response);
-        // var results = response.data;
-        var results = response.data;
-        
-        for (var i = 0; i < results.length; i++) {
+    $("#save-gif").on("click", function(event) {
+        event.preventDefault();
+        $("#button-row").empty();
+        var newGIF = $("#input").val().trim();
+        topic.push(newGIF);
+        renderButtons();
+    })
+
+    function populateGIFs(arrOfObjects){
+        for (var i = 0; i < arrOfObjects.length; i++) {
             var gifDiv = $("<div>");
             
-            var rating = results[i].rating;
-            var title = results[i].title;
+            var rating = arrOfObjects[i].rating;
+            var title = arrOfObjects[i].title;
             
             var p = $("<p>").text("Title: " + title + " " + "Rating: " + rating);
             
@@ -46,32 +34,55 @@ $(document).on("click", ".buttons", function(){
             // animatedGIF = 
             gifImage.addClass("gif")
             gifImage.attr("data-state", "still");
-            gifImage.attr("data-animate", results[i].images.fixed_height.url);
-            gifImage.attr("data-still", results[i].images.fixed_height_still.url);
+            gifImage.attr("data-animate", arrOfObjects[i].images.fixed_height.url);
+            gifImage.attr("data-still", arrOfObjects[i].images.fixed_height_still.url);
             //do variable for images source
-            gifImage.attr("src", results[i].images.fixed_height_still.url);
+            gifImage.attr("src", arrOfObjects[i].images.fixed_height_still.url);
             // animatedGIF.attr("state", "animated")
             // stillGIF.attr("state", "still");
             //attach both static and animated url as attributes
-            
+            var b = $("<br>");
             gifDiv.prepend(p);
             gifDiv.prepend(gifImage);
+            gifDiv.append(b);
             
             $("#gifs-go-here").prepend(gifDiv);
         }
+    };
+
+
+    // var animatedGIF;
+    // var stillGIF;
+    var gifImage;
+    $(document).on("click", ".buttons", function(){
+        var offset = parseInt($(this).attr("data-click"))*10;
+        var count = parseInt($(this).attr("data-click"))
+        var search = $(this).attr("data-title")
+        var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + search + "&offset=" + offset + "&api_key=spWh9C8hZ8YYZbe5GmdARig3L7FNyNFK&limit=10&rating=g&rating=pg";
+        count++
+        $(this).attr("data-click", count);
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
+        }).then(function(response){
+            console.log(response);
+            // var results = response.data;
+            var results = response.data;
+            populateGIFs(results);
+            
+        });
     });
-});
-
-
-$(document).on("click", ".gif", function(){
-    var gifState = $(this).attr("data-state");
-    console.log(gifState);
-    if ( gifState == "still"){
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animated");
-    } else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
-    }
-});
-});
+    
+    
+    $(document).on("click", ".gif", function(){
+        var gifState = $(this).attr("data-state");
+        console.log(gifState);
+        if ( gifState == "still"){
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animated");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    });
+    });
